d9a9849422efbbb5a55332f6d5425c8b76199f03
gabrielmds222 <gabrielmedsilva@outlook.com>
2022-12-18 22:45:01 -0300

    feat: Adiciona model e controller de usuário

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,8 @@
+import { IGetUsersController } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+  handle() {
+    // Validar requisição
+    // direcionar chamada para o repository
+  }
+}
---
+++ src/controllers/get-users/protocols.ts
@@ -0,0 +1,9 @@
+import { User } from "../../models/user";
+
+export interface IGetUsersController {
+  handle(): any;
+}
+
+export interface IGetUsersRepository {
+  getUsers(): Promise<User[]>;
+}
